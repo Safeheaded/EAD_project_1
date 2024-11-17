714a247271b46fcd2899d9f17c1e2f805497fe61
Patryk Marczak <m.patryk@outlook.com>
2024-11-17 08:45:18 +0100

    finished fifth task

--- main.py
+++ main.py
@@ -14,22 +14,38 @@ def get_txt_dataset() -> pd.DataFrame:
     paths = get_file_paths("./data")
 
     column_names = ["Name", "Gender", "Count"]
-    dataframes = [pd.read_csv(path, header=None, names=column_names) for path in paths]
+    dataframes = []
+    for path in paths:
+        df = pd.read_csv(path, header=None, names=column_names)
+        year = os.path.basename(path).split('yob')[1].split('.')[0]
+        df['Year'] = int(year)
+        dataframes.append(df)
     combined_df = pd.concat(dataframes, ignore_index=True)
     return combined_df
 
-def zad1(df: pd.DataFrame):
+def zad2(df: pd.DataFrame):
     unique_names = set(df.loc[:, "Name"])
     print(f"Number of unique names: {len(unique_names)}")
 
-def zad2(df: pd.DataFrame):
+def zad3(df: pd.DataFrame):
     unique_female_names = set(df.loc[df["Gender"] == "F", "Name"])
     unique_male_names = set(df.loc[df["Gender"] == "M", "Name"])
     print(f"Number of unique male names: {len(unique_male_names)}")
     print(f"Number of unique female names: {len(unique_female_names)}")
 
+def zad4(df: pd.DataFrame):
+    counted_births = df.groupby(["Year", "Gender"]).agg({"Count": "sum"})
+    counted_births = counted_births.rename(columns={"Count": "General_count"})
+
+    df = df.merge(counted_births, on=["Year", "Gender"])
+    df['female_frequency'] = (df['Count'] / df['General_count']).mask(df['Gender'] == 'M', 0)
+    df['male_frequency'] = (df['Count'] / df['General_count']).mask(df['Gender'] == 'F', 0)
+
+    print(df)
+
 def main():
     df = get_txt_dataset()
+    zad4(df)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
