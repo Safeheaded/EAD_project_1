422c32bcb26729d21f8ed42aeb81b47306c37f6d
Patryk Marczak <m.patryk@outlook.com>
2024-11-28 11:34:57 +0100

    Finito

--- main.py
+++ main.py
@@ -45,8 +45,8 @@ def zad4(df: pd.DataFrame):
     counted_births = counted_births.rename(columns={"Count": "General_count"})
 
     df = df.merge(counted_births, on=["Year", "Gender"])
-    df['female_frequency'] = (df['Count'] / df['General_count']).mask(df['Gender'] == 'M', 0)
-    df['male_frequency'] = (df['Count'] / df['General_count']).mask(df['Gender'] == 'F', 0)
+    df['frequency_female'] = (df['Count'] / df['General_count']).mask(df['Gender'] == 'M', 0)
+    df['frequency_male'] = (df['Count'] / df['General_count']).mask(df['Gender'] == 'F', 0)
 
     print(df)
 
@@ -79,38 +79,46 @@ def zad5(df: pd.DataFrame):
     plt.show()
 
     print(f'Year with the highest difference in births: {highest_diff_year}')
-    print(f"Value closest to 1: {closest_to_one_value} at index {closest_to_one_index}")
+    print(f'Year with the smallest difference in births: {closest_to_one_year}')
 
-def zad6(df: pd.DataFrame):
-    gender_counts = df.groupby(["Year", "Gender"]).agg({"Count": "sum"}).rename(columns={"Count": "Total_count"})
+def zad6(df: pd.DataFrame, names_num=1000, split_by_gender=True, should_display=True):
+    counts_columns = ["Year", "Gender"] if split_by_gender else ["Year"]
+    gender_counts = df.groupby(counts_columns).agg({"Count": "sum"}).rename(columns={"Count": "Total_count"})
     # gender_counts = gender_counts.reset_index()
 
     # female_counts = gender_counts.loc[(slice(None), 'F'), 'Count'].reset_index()
     # male_counts = gender_counts.loc[(slice(None), 'M'), 'Count'].reset_index()
-
-    counted = df.groupby(["Year", "Gender", "Name"]).agg({"Count": "sum"}).reset_index()
+    counted_columns = ["Year", "Gender", "Name"] if split_by_gender else ["Year", "Name"]
+    counted = df.groupby(counted_columns).agg({"Count": "sum"}).reset_index()
     counted = counted
 
-    counted = gender_counts.merge(counted, on=["Year", "Gender"])
+    counted = gender_counts.merge(counted, on=counts_columns)
     # print(counted)
 
     counted["popularity"] = counted["Count"] / counted["Total_count"]
 
     with_popularity = counted.copy()
 
-    counted = counted.groupby(["Gender", "Name"]).agg({"popularity": "sum"}).reset_index()
+    counted_second_columns = ["Gender", "Name"] if split_by_gender else ["Name"]
+    counted = counted.groupby(counted_second_columns).agg({"popularity": "sum"}).reset_index()
 
 
-    females = counted[counted["Gender"] == "F"]
-    males = counted[counted["Gender"] == "M"]
+    females = counted[counted["Gender"] == "F"] if split_by_gender else counted
+    males = counted[counted["Gender"] == "M"] if split_by_gender else counted
 
-    top_1000_females = females.sort_values(by="popularity", ascending=False).head(1000)
-    top_1000_males = males.sort_values(by="popularity", ascending=False).head(1000)
+    top_1000_females = females.sort_values(by="popularity", ascending=False).head(names_num)
+    top_1000_males = males.sort_values(by="popularity", ascending=False).head(names_num)
+
+    if should_display:
+        print(f"Top {names_num} females:")
+        print(top_1000_females)
+        print(f"Top {names_num} males:")
+        print(top_1000_males)
 
     return top_1000_males, top_1000_females, with_popularity
 
 def zad7(df: pd.DataFrame):
-    top_males, top_females, all_top_1000 = zad6(df)
+    top_males, top_females, all_top_1000 = zad6(df, should_display=False)
     male_name = 'John'
     female_name = top_females.iloc[0, 1]
 
@@ -157,52 +165,64 @@ def zad7(df: pd.DataFrame):
     fig.tight_layout()
     plt.show()
 
-def zad8(df: pd.DataFrame):
-    top_males, top_females, all_top_1000 = zad6(df)
-    counted_births = df.groupby(["Year", "Name", "Gender"]).agg({"Count": "sum"})
-    counted_births["total_yearly_population"] = counted_births.groupby(["Year", "Gender"])["Count"].transform('sum')
+def zad8(df: pd.DataFrame, names_num=1000, split_by_gender=True, should_display=True):
+    top_males, top_females, all_top_1000 = zad6(df, names_num, split_by_gender, should_display=False)
+    counted_births_columns = ["Year", "Name", "Gender"] if split_by_gender else ["Year", "Name"]
+    counted_births = df.groupby(counted_births_columns).agg({"Count": "sum"})
+    total_columns = ["Year", "Gender"] if split_by_gender else ["Year"]
+    counted_births["total_yearly_population"] = counted_births.groupby(total_columns)["Count"].transform('sum')
 
     top_male_names = top_males['Name'].tolist()
     top_female_names = top_females['Name'].tolist()
 
-    counted_births_females = counted_births.loc[(slice(None), slice(None), "F"), :]
-    counted_births_males = counted_births.loc[(slice(None), slice(None), "M"), :]
+    counted_births_females = counted_births.loc[(slice(None), slice(None), "F"), :] if split_by_gender else counted_births
+    counted_births_males = counted_births.loc[(slice(None), slice(None), "M"), :] if split_by_gender else counted_births
 
-    # Musisz to przefiltrować oddzielnie dla bab i chłopów
     counted_births_females_filtered = counted_births_females[counted_births_females.index.get_level_values('Name').isin(top_female_names)]
     counted_births_males_filtered = counted_births_males[counted_births_males.index.get_level_values('Name').isin(top_male_names)]
 
-    counted_births_females_filtered["population_percentage"] = counted_births_females_filtered["Count"] / counted_births_females_filtered["total_yearly_population"]
-    counted_births_males_filtered["population_percentage"] = counted_births_males_filtered["Count"] / counted_births_males_filtered["total_yearly_population"]
+    counted_births_females_filtered.loc[:,"population_percentage"] = counted_births_females_filtered["Count"] / counted_births_females_filtered["total_yearly_population"]
+    counted_births_males_filtered.loc[:,"population_percentage"] = counted_births_males_filtered["Count"] / counted_births_males_filtered["total_yearly_population"]
 
     females_aggregated = counted_births_females_filtered.groupby("Year").agg({"population_percentage": "sum"})
     males_aggregated = counted_births_males_filtered.groupby("Year").agg({"population_percentage": "sum"})
 
     # Plotting
     plt.figure(figsize=(10, 6))
-    plt.plot(females_aggregated.index, females_aggregated["population_percentage"], label="Females")
-    plt.plot(males_aggregated.index, males_aggregated["population_percentage"], label="Males")
+    plt.plot(females_aggregated.index, females_aggregated["population_percentage"], label="Females" if split_by_gender else "Percentage of top names")
+    if split_by_gender:
+        plt.plot(males_aggregated.index, males_aggregated["population_percentage"], label="Males")
     plt.xlabel("Year")
     plt.ylabel("Population Percentage")
     plt.title("Population Percentage of Top Names by Year")
-    print(females_aggregated)
-    females_aggregated["diff"] = abs(females_aggregated.loc[:, "population_percentage"] - males_aggregated.loc[:,"population_percentage"])
-    biggest_diff_year = females_aggregated.reset_index().iloc[argmax(females_aggregated["diff"]), 0]
+    if split_by_gender:
+        females_aggregated["diff"] = abs(
+            females_aggregated.loc[:, "population_percentage"] - males_aggregated.loc[:, "population_percentage"])
+        biggest_diff_year = females_aggregated.reset_index().iloc[argmax(females_aggregated["diff"]), 0]
 
-    min_val = min(females_aggregated["population_percentage"])
-    min_val_male = min(males_aggregated["population_percentage"])
-    min_min = min(min_val, min_val_male)
+        min_val = min(females_aggregated["population_percentage"])
+        min_val_male = min(males_aggregated["population_percentage"])
+        min_min = min(min_val, min_val_male)
 
-    # plt.axvline(x=biggest_diff_year, color='r', linestyle='--')
-    plt.scatter(biggest_diff_year, females_aggregated.loc[biggest_diff_year, "population_percentage"], color='r', label="Biggest difference year")
-    plt.scatter(biggest_diff_year, males_aggregated.loc[biggest_diff_year, "population_percentage"], color='r')
-    plt.vlines(x=biggest_diff_year, ymin=min_min, ymax=females_aggregated.loc[biggest_diff_year, "population_percentage"], color='r', linestyle='--')
-    plt.vlines(x=biggest_diff_year, ymin=min_min, ymax=males_aggregated.loc[biggest_diff_year, "population_percentage"], color='r', linestyle='--')
+        # plt.axvline(x=biggest_diff_year, color='r', linestyle='--')
+        plt.scatter(biggest_diff_year, females_aggregated.loc[biggest_diff_year, "population_percentage"], color='r',
+                    label="Biggest difference year")
+        plt.scatter(biggest_diff_year, males_aggregated.loc[biggest_diff_year, "population_percentage"], color='r')
+        plt.vlines(x=biggest_diff_year, ymin=min_min,
+                   ymax=females_aggregated.loc[biggest_diff_year, "population_percentage"], color='r', linestyle='--')
+        plt.vlines(x=biggest_diff_year, ymin=min_min,
+                   ymax=males_aggregated.loc[biggest_diff_year, "population_percentage"], color='r', linestyle='--')
+        print(f"Year with the biggest difference in population percentage: {biggest_diff_year}")
 
     plt.legend()
     plt.show()
 
-    print(f"Year with the biggest difference in population percentage: {biggest_diff_year}")
+    if should_display:
+        print("Różnorodność wśród imion rosła do 1910 roku. Malała w latach 1910-1960 i zaczęła bardzo rosnąć od 1960 roku.")
+        print("Różnorodność wśród imion zaley od płci.")
+
+    return top_males, top_females, all_top_1000
+
 
 def zad9(df: pd.DataFrame):
     df["last_letter"] = df["Name"].str[-1]
@@ -283,49 +303,9 @@ def zad9(df: pd.DataFrame):
     plt.legend()
     plt.show()
 
-def zad10(df: pd.DataFrame):
-    top_1000_males, top_1000_females, _ = zad6(df)
-    male_names = top_1000_males["Name"].tolist()
-    female_names = top_1000_females["Name"].tolist()
-    common_names = list(set(male_names) & set(female_names))
-
-    data = df.loc[df["Name"].isin(common_names)]
-
-    data_total_names = data.groupby(["Year", "Name"]).agg({"Count": "sum"}).rename(columns={"Count": "total_count"}).reset_index()
-    data = data.merge(data_total_names, on=["Year", "Name"])
-    data["ratio"] = data["Count"] / data["total_count"]
-
-    # data["shifted_ratio"] = data["ratio"].shift(1)
-
-    first_period = data.loc[data["Year"].between(1880, 1919)]
-
-    first_period_males = first_period[first_period["Gender"] == "M"]
-    first_period_females = first_period[first_period["Gender"] == "F"]
-
-    first_period_males_grouped = first_period_males.groupby(["Name"]).agg({"ratio": "mean"})
-    first_period_females_grouped = first_period_females.groupby(["Name"]).agg({"ratio": "mean"})
-
-    # print(pd.crosstab(index=[first_period_males["Name"], first_period_males["Year"]], columns=first_period_males["Year"], values=first_period_males["ratio"], aggfunc='mean'))
-
-    second_period = data.loc[data["Year"].between(2001, 2023)]
-
-    second_period_males = second_period[second_period["Gender"] == "M"]
-    second_period_females = second_period[second_period["Gender"] == "F"]
-
-    second_period_males_grouped = second_period_males.groupby(["Name"]).agg({"ratio": "mean"})
-    second_period_females_grouped = second_period_females.groupby(["Name"]).agg({"ratio": "mean"})
-
-    mr = first_period_males_grouped.merge(second_period_females_grouped, on=["Name"])
-    mr2 = first_period_females_grouped.merge(second_period_males_grouped, on=["Name"])
-
-    mr["fin"] = (mr["ratio_x"] + mr["ratio_y"]) / 2
-    mr2["fin"] = (mr2["ratio_x"] + mr2["ratio_y"]) / 2
-
-    now_female_name = mr["fin"].sort_values(ascending=False).head(1).reset_index()["Name"]
-    now_male_name = mr2["fin"].sort_values(ascending=False).head(1).reset_index()["Name"]
-
-    print(now_female_name)
-    print(now_male_name)
+def zad10(df: pd.DataFrame, names_num=1000):
+    top_1000_males, top_1000_females, _ = zad6(df, names_num, should_display=False)
+    data, now_female_name, now_male_name = get_changing_names(df, top_1000_females, top_1000_males)
 
     males = data[data["Gender"] == "M"]
     females = data[data["Gender"] == "F"]
@@ -337,6 +317,9 @@ def zad10(df: pd.DataFrame):
 
     now_female_data = comp[comp["Name"] == now_female_name[0]]
     now_male_data = comp[comp["Name"] == now_male_name[0]]
+
+    print(f"Kiedyś męskie, obecnie żeńskie imię: {now_female_name[0]}")
+    print(f"Kiedyś żeńskie, obecnie męskie imię: {now_male_name[0]}")
 
     plt.figure(figsize=(10, 6))
     plt.plot(now_female_data["Year"], now_female_data["ratio"], marker='o', linestyle='-', label=f"Popularity of name {now_female_name[0]}")
@@ -353,6 +336,47 @@ def zad10(df: pd.DataFrame):
 
     plt.show()
 
+
+def get_changing_names(df, top_1000_females, top_1000_males):
+    data = get_gender_popularity(df, top_1000_females, top_1000_males)
+    # data["shifted_ratio"] = data["ratio"].shift(1)
+    first_period = data.loc[data["Year"].between(1880, 1919)]
+    first_period_females_grouped, first_period_males_grouped = prepare_gender_popularity_period(first_period)
+    # print(pd.crosstab(index=[first_period_males["Name"], first_period_males["Year"]], columns=first_period_males["Year"], values=first_period_males["ratio"], aggfunc='mean'))
+    second_period = data.loc[data["Year"].between(2001, 2023)]
+    second_period_males = second_period[second_period["Gender"] == "M"]
+    second_period_females = second_period[second_period["Gender"] == "F"]
+    second_period_males_grouped = second_period_males.groupby(["Name"]).agg({"ratio": "mean"})
+    second_period_females_grouped = second_period_females.groupby(["Name"]).agg({"ratio": "mean"})
+    mr = first_period_males_grouped.merge(second_period_females_grouped, on=["Name"])
+    mr2 = first_period_females_grouped.merge(second_period_males_grouped, on=["Name"])
+    mr["fin"] = (mr["ratio_x"] + mr["ratio_y"]) / 2
+    mr2["fin"] = (mr2["ratio_x"] + mr2["ratio_y"]) / 2
+    now_female_name = mr["fin"].sort_values(ascending=False).head(1).reset_index()["Name"]
+    now_male_name = mr2["fin"].sort_values(ascending=False).head(1).reset_index()["Name"]
+    return data, now_female_name, now_male_name
+
+
+def prepare_gender_popularity_period(first_period):
+    first_period_males = first_period[first_period["Gender"] == "M"]
+    first_period_females = first_period[first_period["Gender"] == "F"]
+    first_period_males_grouped = first_period_males.groupby(["Name"]).agg({"ratio": "mean"})
+    first_period_females_grouped = first_period_females.groupby(["Name"]).agg({"ratio": "mean"})
+    return first_period_females_grouped, first_period_males_grouped
+
+
+def get_gender_popularity(df, top_1000_females, top_1000_males):
+    male_names = top_1000_males["Name"].tolist()
+    female_names = top_1000_females["Name"].tolist()
+    common_names = list(set(male_names) & set(female_names))
+    data = df.loc[df["Name"].isin(common_names)]
+    data_total_names = data.groupby(["Year", "Name"]).agg({"Count": "sum"}).rename(
+        columns={"Count": "total_count"}).reset_index()
+    data = data.merge(data_total_names, on=["Year", "Name"])
+    data["ratio"] = data["Count"] / data["total_count"]
+    return data
+
+
 def zad11():
     conn = sqlite3.connect("./data/names_pl_2000-23.sqlite")  # połączenie do bazy danych - pliku
     df = pd.read_sql_query("SELECT * FROM males UNION ALL SELECT * FROM females", conn)
@@ -360,7 +384,46 @@ def zad11():
     df['Gender'] = df['Gender'].replace('K', 'F')
     conn.close()
 
-    print(df)
+    # Getting 200 most popular names:
+
+    top_names, _, all_top = zad8(df, 200, False, should_display=False)
+
+    names = list(set(top_names["Name"]))
+    top_names = all_top.loc[all_top["Name"].isin(names)]
+
+    years = [2000, 2013, 2023]
+
+    filtered_data = top_names[top_names["Year"].isin(years)]
+
+    # Plot the histogram
+    fig, axes = plt.subplots(1, 3, figsize=(10, 6))
+    plt.xlabel("Popularity")
+    plt.ylabel("Frequency")
+    colors = ['blue', 'green', 'red']
+    for i, year in enumerate(years):
+        yearly_data = filtered_data[filtered_data["Year"] == year]
+        axes[i].hist(yearly_data["popularity"], bins=20, alpha=0.5, label=str(year), color=colors[i])
+        axes[i].legend()
+        axes[i].set_ylim(0, 120)
+
+    plt.title("Histogram of Name Popularity for Selected Years")
+
+    plt.show()
+
+    print("Możemy zauważyc podobne trendy w porównaniu z USA, lecz wzrost zróznicowania nie jest aż tak duży jak w USA.")
+    print("Analizujac histogram mozemy zauważyc, że na przestrzeni lat zmniejszyła się ilość nadawania bardzo popularnych imion")
+    print("Zmiana może wynikać z wstąpienia do Uni Europejskiej, napływu imigrantów, silnym wpływie kultury zachodniej i amerykańskiej")
+
+
+    # Getting most gender neutral names:
+
+    data = get_gender_popularity(df, df[df["Gender"] == "F"], df[df["Gender"] == "M"])
+    first_period_females_grouped, first_period_males_grouped = prepare_gender_popularity_period(data)
+    mr = first_period_males_grouped.merge(first_period_males_grouped, on=["Name"])
+    mr["fin"] = (mr["ratio_x"] + mr["ratio_y"]) / 2
+    mr = mr.sort_values(by="fin", ascending=False).head(2)
+    print("Imiona często nadawane chłopcom i dziewczynkom:")
+    print(mr.reset_index()["Name"])
 
 def main():
     df = get_txt_dataset()
